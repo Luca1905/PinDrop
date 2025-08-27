7bd760f51dfe2f30eec1fe05f3ba824a33b39a32
Luca Wang <contact@lucwang.me>
2025-08-28 00:37:19 +0800

    use prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,12 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -23,4 +24,5 @@ export default tseslint.config([
 
     }
   },
+  eslintConfigPrettier
 ])
